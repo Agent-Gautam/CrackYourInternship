955b73135f069902d69fbd4abf32abd986c6acfd
Gautam <gautamanand931@gmail.com>
2024-07-15 16:06:13 +0530

    question2 - chocolate distribution

--- chocolateDistribution.cpp
+++ chocolateDistribution.cpp
@@ -1,15 +1,42 @@
-class Solution {
-public:
-    int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        int maxprofit = 0, minimum = prices[0];
-        for(int i = 1; i < n; i++) {
-            int curprofit = prices[i] - minimum;
-            if(curprofit > maxprofit) {
-                maxprofit = curprofit;
-            }
-            minimum = min(minimum, prices[i]);
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    long long findMinDiff(vector<long long> a, long long n, long long m){
+        sort(a.begin(), a.end());
+        long long i = 0, j = m-1;
+        long long mindiff = a[j] - a[i];
+        i++; j++;
+        while(j < n) {
+            if(a[j] - a[i] < mindiff) mindiff = a[j] - a[i];
+            i++; j++;
         }
-        return maxprofit;
-    }
+        return mindiff;
+    }   
 };
+
+int main() {
+	long long t;
+	cin>>t;
+	while(t--)
+	{
+		long long n;
+		cin>>n;
+		vector<long long> a;
+		long long x;
+		for(long long i=0;i<n;i++)
+		{
+			cin>>x;
+			a.push_back(x);
+		}
+		
+		long long m;
+		cin>>m;
+		Solution ob;
+		cout<<ob.findMinDiff(a,n,m)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
